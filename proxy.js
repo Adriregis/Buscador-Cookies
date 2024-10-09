9782e4ba51dce09b24a68371a99c5afbb219b0ed
Adriana <aadriana.regis@gmail.com>
2024-10-09 17:07:55 -0300

    Finalizado

---
+++ proxy.js
@@ -0,0 +1,34 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+
+app.get("/search", async (req, res) => {
+  const { query } = req.query
+  const API_KEY = ""
+  const URL = "https://serpapi.com/search.json";
+  try {
+    const response = await axios.get(URL, {
+      params: {
+        q: query,
+        engine: "google",
+        google_domain: "google.com.br",
+        api_key: API_KEY,
+        hl: "pt-br",
+        gl: "br",
+        num: 10, 
+      },
+    });
+    res.json(response.data);
+  } catch (err) {
+    res.status(500).json({ error: "Erro ao buscar resultados" });
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Proxy rodando na porta`, porta);
+})
